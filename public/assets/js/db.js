623cf4496877bfd0ec4f9bd2de2c505c1adabf03
anhnguyendon92 <anhnguyendon92@gmail.com>
2020-12-12 13:26:42 -0800

    Coding

--- public/assets/js/db.js
+++ public/assets/js/db.js
@@ -1,151 +1,76 @@
-let transactions = [];
-let myChart;
+let db;
+// create a new db request for a "budget" database.
+const request = window.indexedDB.open("budget", 1);
 
-fetch("/api/transaction")
-    .then(response => {
-        return response.json();
-    })
-    .then(data => {
-        // save db data on global variable
-        transactions = data;
+request.onupgradeneeded = function(event) {
+    // create object store called "pending" and set autoIncrement to true
+    db = event.target.result;
 
-        populateTotal();
-        populateTable();
-        populateChart();
+    const pendingStore = db.createObjectStore("pending", {
+        autoIncrement: true
     });
 
-function populateTotal() {
-    // reduce transaction amounts to a single total value
-    let total = transactions.reduce((total, t) => {
-        return total + parseInt(t.value);
-    }, 0);
+};
 
-    let totalEl = document.querySelector("#total");
-    totalEl.textContent = total;
-}
+request.onsuccess = function(event) {
+    db = event.target.result;
 
-function populateTable() {
-    let tbody = document.querySelector("#tbody");
-    tbody.innerHTML = "";
-
-    transactions.forEach(transaction => {
-        // create and populate a table row
-        let tr = document.createElement("tr");
-        tr.innerHTML = `
-      <td>${transaction.name}</td>
-      <td>${transaction.value}</td>
-    `;
-
-        tbody.appendChild(tr);
-    });
-}
-
-function populateChart() {
-    // copy array and reverse it
-    let reversed = transactions.slice().reverse();
-    let sum = 0;
-
-    // create date labels for chart
-    let labels = reversed.map(t => {
-        let date = new Date(t.date);
-        return `${date.getMonth() + 1}/${date.getDate()}/${date.getFullYear()}`;
-    });
-
-    // create incremental values for chart
-    let data = reversed.map(t => {
-        sum += parseInt(t.value);
-        return sum;
-    });
-
-    // remove old chart if it exists
-    if (myChart) {
-        myChart.destroy();
+    if (navigator.onLine) {
+        checkDatabase();
     }
+};
 
-    let ctx = document.getElementById("myChart").getContext("2d");
+request.onerror = function(event) {
+    // log error here
+    console.log(request.errorCode);
+};
 
-    myChart = new Chart(ctx, {
-        type: 'line',
-        data: {
-            labels,
-            datasets: [{
-                label: "Total Over Time",
-                fill: true,
-                backgroundColor: "#6666ff",
-                data
-            }]
+// save transactions to indexed DB when offline
+function saveRecord(record) {
+    // create a transaction on the pending db with readwrite access
+    // access the pending object store
+    // add record to the store with add method.
+    const transaction = db.transaction(["pending"], "readwrite");
+    const pendingStore = transaction.objectStore("pending");
+    console.log("Offline - Saving Transaction in IndexedDB");
+    console.log(record);
+    pendingStore.add(record);
+}
+
+function checkDatabase() {
+    // open a transaction on the pending db
+    // access the pending object store
+    // get all records from store and set to a variable
+    const transaction = db.transaction(["pending"], "readwrite");
+    const pendingStore = transaction.objectStore("pending");
+
+    const getAll = pendingStore.getAll();
+
+    // post transations stored while offline to the database when online access is restured
+    getAll.onsuccess = function() {
+        if (getAll.result.length > 0) {
+            fetch('/api/transaction/bulk', {
+                    method: 'POST',
+                    body: JSON.stringify(getAll.result),
+                    headers: {
+                        Accept: 'application/json, text/plain, */*',
+                        'Content-Type': 'application/json',
+                    },
+                })
+                .then((response) => response.json())
+                .then(() => {
+                    // if successful, open a transaction on the pending db
+                    // access the pending object store
+                    // clear all items in the store
+                    const transaction = db.transaction(["pending"], "readwrite");
+                    const pendingStore = transaction.objectStore("pending");
+                    console.log("Online - Post Transactions and Clear IndexedDB");
+                    pendingStore.clear();
+
+                });
         }
-    });
-}
-
-function sendTransaction(isAdding) {
-    let nameEl = document.querySelector("#t-name");
-    let amountEl = document.querySelector("#t-amount");
-    let errorEl = document.querySelector(".form .error");
-
-    // validate form
-    if (nameEl.value === "" || amountEl.value === "") {
-        errorEl.textContent = "Missing Information";
-        return;
-    } else {
-        errorEl.textContent = "";
-    }
-
-    // create record
-    let transaction = {
-        name: nameEl.value,
-        value: amountEl.value,
-        date: new Date().toISOString()
     };
-
-    // if subtracting funds, convert amount to negative number
-    if (!isAdding) {
-        transaction.value *= -1;
-    }
-
-    // add to beginning of current array of data
-    transactions.unshift(transaction);
-
-    // re-run logic to populate ui with new record
-    populateChart();
-    populateTable();
-    populateTotal();
-
-    // also send to server
-    fetch("/api/transaction", {
-            method: "POST",
-            body: JSON.stringify(transaction),
-            headers: {
-                Accept: "application/json, text/plain, */*",
-                "Content-Type": "application/json"
-            }
-        })
-        .then(response => {
-            return response.json();
-        })
-        .then(data => {
-            if (data.errors) {
-                errorEl.textContent = "Missing Information";
-            } else {
-                // clear form
-                nameEl.value = "";
-                amountEl.value = "";
-            }
-        })
-        .catch(err => {
-            // fetch failed, so save in indexed db
-            saveRecord(transaction);
-
-            // clear form
-            nameEl.value = "";
-            amountEl.value = "";
-        });
 }
 
-document.querySelector("#add-btn").onclick = function() {
-    sendTransaction(true);
-};
-
-document.querySelector("#sub-btn").onclick = function() {
-    sendTransaction(false);
-};
+// listen for app coming back online
+window.addEventListener('online', checkDatabase);
